48dcedcef5e8d96888310656be39733b2d574d0c
Govindarajan D <rdgovindarajan@gmail.com>
2022-10-01 19:59:22 +0530

    LogFile creation to explore the output metrics

--- src/main/scala/com/sparklistenermetrics/metrics/SparkListenerMetrics.scala
+++ src/main/scala/com/sparklistenermetrics/metrics/SparkListenerMetrics.scala
@@ -4,11 +4,13 @@ import org.apache.spark.scheduler.{SparkListener,TaskInfo, SparkListenerJobEnd, 
 import org.apache.spark.executor.TaskMetrics
 import scala.collection.mutable.Buffer
 import java.util.logging.Logger
+import java.io.PrintWriter
 import org.slf4j.LoggerFactory
 
 class SparkListenerMetrics extends SparkListener {
   private val logger = LoggerFactory.getLogger(getClass.getName)
   private val TaskDetails = Buffer[(TaskInfo,TaskMetrics)]()
+  private val logWriter = new PrintWriter("log.txt")
 
   override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
      var taskMetricsVar = taskEnd.taskMetrics
@@ -17,6 +19,7 @@ class SparkListenerMetrics extends SparkListener {
   }
 
   override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
+    val TaskDetailsString: Seq[String] = TaskDetails.map{temp:(TaskInfo,TaskMetrics) => TaskTimeAgg(temp._1,temp._2)}
     logger.info("OnStageCompleted - RecordsWritten:" + stageCompleted.stageInfo.taskMetrics.outputMetrics.recordsWritten)
     logger.info("Stage Details:"+stageCompleted.stageInfo.details)
     logger.info(""+stageCompleted.stageInfo.toString)
@@ -24,5 +27,12 @@ class SparkListenerMetrics extends SparkListener {
 
   override def onJobEnd(jobEnd: SparkListenerJobEnd): Unit = {
     logger.info("OnJobEnd" + jobEnd.jobResult.toString())
+    logWriter.close()
+  }
+
+  def TaskTimeAgg(info: TaskInfo, metrics: TaskMetrics): String ={
+    logWriter.write(info.toString)
+    logWriter.write(metrics.toString)
+    info.toString
   }
 }
